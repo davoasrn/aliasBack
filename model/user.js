1f22faec8ffb7af1b9b81c9a1e34465a8eec097c
davoasrn <davoasrn@gmail.com>
2021-06-13 19:43:59 +0400

    changes in user model, added new columns

--- model/user.js
+++ model/user.js
@@ -4,28 +4,53 @@ const crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 var secret = require('../config').secret;
 
-
-
 const user = new mongoose.Schema({
-  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-  email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},  bio: String,
+  firstName: {
+    type: String,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    trim: true
+  },
+  username: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    trim: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    trim: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true
+  },
+  bio: String,
   image: String,
   hash: String,
-  salt: String
+  salt: String,
+  isEnabled: {
+    type: Boolean,
+  }
 });
 
-user.plugin(uniqueValidator, {message: 'is already taken.'});
+user.plugin(uniqueValidator, { message: 'is already taken.' });
 
-user.methods.setPassword = function(password) {
+user.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 }
 
-user.methods.validPassword = function(password) {
+user.methods.validPassword = function (password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
   return this.hash === hash;
 }
 
-mongoose.model('User', user);
-
-module.exports user;
+module.exports = mongoose.model('user', user);
+;
